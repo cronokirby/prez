f5240b08b90dc5af00f3e2e825a0576f796cabda
Lúcás Meier <cronokirby@gmail.com>
2020-07-17 11:52:16 +0200

    Add a full model using state information

--- predict.py
+++ predict.py
@@ -1,71 +1,76 @@
-from dataclasses import dataclass
 import numpy as np
 import matplotlib.pyplot as plt
 import pymc3 as pm
-import csv
+import pandas as pd
 
 
 def percent_from_str(s):
     return float(s.strip("%")) / 100
 
 
-@dataclass
-class PollData:
-    """
-    Represents aggregate poll data.
-
-    size: the total number of people polled
-    count_D: the total number of Democratic choices
-    count_R: the total number of Republican choices
-    count_O: the total number of choices for another candidate / undecided
-    """
-
-    size: int
-    count_D: int
-    count_R: int
-    count_O: int
-
-    @staticmethod
-    def from_csv(path):
-        with open(path, "r") as fp:
-            reader = csv.reader(fp, delimiter=",")
-            total = 0
-            total_D = 0
-            total_R = 0
-            for row in reader:
-                # We only want the national counts, for now
-                if row[0] != "National":
-                    continue
-                poll_count = int(row[1])
-                percent_D = percent_from_str(row[3])
-                percent_R = percent_from_str(row[4])
-                total += poll_count
-                total_D += poll_count * percent_D
-                total_R += poll_count * percent_R
-            total_D = int(total_D)
-            total_R = int(total_R)
-            return PollData(total, total_D, total_R, total - total_D - total_R)
+def read_state_info(csv_file):
+    df = pd.read_csv(csv_file)
+    df.D_2016 = df.D_2016.apply(percent_from_str)
+    df.R_2016 = df.R_2016.apply(percent_from_str)
+    df["D_Prob"] = df.D_2016 / (df.D_2016 + df.R_2016)
+    df["Weight"] = df.Adults / df.Adults.sum()
+    # Making sure we have a consistent order later
+    return df.sort_values(by=["State"])
 
 
-SAMPLE_SIZE = 1000
+def get_state_probs(df):
+    return np.array(df[df.State != "Popular"].D_Prob)
+
+
+def get_state_weights(df):
+    return np.array(df[df.State != "Popular"].Weight)
+
+
+def get_state_electors(df):
+    return np.array(df[df.State != "Popular"].Electors)
+
+
+def get_popular_percentage(df):
+    return df[df.State == "Popular"].D_Prob[0]
+
+
+def logit(p):
+    return np.log(p) - np.log(1 - p)
+
+
+# The variance for the logits of probabilities
+VARIANCE = 0.05
+# The correlation between logits of different states
+STATE_CORR = 0.5
 
 if __name__ == "__main__":
+    state_df = read_state_info("state_info.csv")
+    state_votes = get_state_probs(state_df)
+    state_weights = get_state_weights(state_df)
+    state_electors = get_state_electors(state_df)
+    # If this isn't 50 something is wrong
+    num_states = len(state_votes)
+    popular_p = get_popular_percentage(state_df)
+
+    print(state_df)
     model = pm.Model()
     with model:
-        mu_a = pm.Normal("mu_a", mu=0, sigma=1)
-        mu_b = pm.Normal("mu_b", mu=0, sigma=1, shape=(4,))
+        mu_a = pm.Normal("mu_a", mu=logit(popular_p), sigma=pm.math.sqrt(VARIANCE))
+        # Gives us a symmetric matrix with the right variances and covariances
+        cov = VARIANCE * np.eye(num_states) + VARIANCE * STATE_CORR * (
+            1 - np.eye(num_states)
+        )
+        mu_b = pm.MvNormal("mu_b", mu=logit(state_votes), cov=cov, shape=(num_states,))
         state_probs = pm.Deterministic("state_probs", pm.math.sigmoid(mu_a + mu_b))
-        texas_results = pm.Binomial("tx_results", 100, state_probs[0], observed=40)
         p_likes_biden = pm.Deterministic(
-            "p_likes_biden", pm.math.dot([0.7, 0.3, 0.2, 0.1], state_probs)
+            "p_likes_biden", pm.math.dot(state_weights, state_probs)
         )
         biden_state_wins = pm.Bernoulli(
-            "biden_state_wins", p=state_probs.flatten(), shape=(4,)
+            "biden_state_wins", p=state_probs.flatten(), shape=(num_states,)
         )
         biden_electors = pm.Deterministic(
-            "biden_electors", pm.math.dot([4, 0, 0, 0], biden_state_wins)
+            "biden_electors", pm.math.dot(state_electors, biden_state_wins)
         )
-        results = pm.Binomial("results", 100, p_likes_biden, observed=60)
         trace = pm.sample()
     pm.plots.plot_posterior(trace, var_names="biden_electors")
     plt.gcf().savefig(".out/electors.png")
@@ -75,4 +80,4 @@ if __name__ == "__main__":
         combined=True,
         colors=["red", "blue", "blue", "blue"],
     )
-    plt.gcf().savefig(".out/posterior.png")
+    plt.gcf().savefig(".out/state_probabilities.png")
